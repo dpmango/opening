b8f28d4318ab2a52426ebfed4409c5e1e4dcc281
Sergey Khmelevskoy <xs290@me.com>
2018-05-12 13:46:36 +0300

    copy video task

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -7,6 +7,12 @@ gulp.task('copy:fonts', function() {
     .pipe(gulp.dest(config.dest.fonts));
 });
 
+gulp.task('copy:video', function() {
+  return gulp
+    .src(config.src.video + '/*.*')
+    .pipe(gulp.dest(config.dest.video));
+});
+
 gulp.task('copy:vendor', function() {
   return gulp
     .src(config.src.vendor + '/**/*.*')
@@ -22,10 +28,12 @@ gulp.task('copy:rootfiles', function() {
 gulp.task('copy', [
   'copy:rootfiles',
   'copy:vendor',
+  'copy:video',
   'copy:fonts'
 ]);
 
 gulp.task('copy:watch', function() {
+  gulp.watch(config.src.video + '/*.*', ['copy:video']);
   gulp.watch(config.src.vendor + '/**/*.*', ['copy:vendor']);
   gulp.watch(config.src.root + '/*.*', ['copy:rootfiles']);
 });
